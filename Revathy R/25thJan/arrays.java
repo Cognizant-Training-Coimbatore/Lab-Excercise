abd80d46bd34590bebfbf9b5bd3fa0b9d518b9dc
Revathy R <60283011+revathyr25@users.noreply.github.com>
2020-01-25 01:48:47 -0800

    Add files via upload

---
+++ Revathy R/25thJan/arrays.java
@@ -0,0 +1,17 @@
+
+public class arrays {
+
+	public static void main(String[] args) 
+	{
+		int n[]=new int[5];
+		n[0]=100;
+		n[1]=21;
+		n[2]=34;
+		n[3]=43;
+		n[4]=78;
+		int sum=n[0]+n[1]+n[2]+n[3]+n[4];
+		System.out.println(sum);
+		
+	}
+
+}
