881c22eabfaf7fb97b45f66ea441d155f5b375bd
ashirjohnson <50360265+ashirjohnson@users.noreply.github.com>
2020-01-25 12:31:14 +0530

    Add files via upload

---
+++ AshirJohnson/21jan/constant.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class constant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        final int x=10;
+        System.out.println(x);
+        x=20;
+        System.out.println(x);
+	}
+
+}
---
+++ AshirJohnson/21jan/declare_variable.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class declare_variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int x=10;
+       System.out.println("The value of X is: "+x);
+	}
+
+}
