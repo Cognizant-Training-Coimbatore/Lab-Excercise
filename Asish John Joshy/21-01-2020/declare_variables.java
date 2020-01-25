cb40bb1b94691a9bba110e74d5b89354d0b8aeba
asishjj007 <42326323+asishjj007@users.noreply.github.com>
2020-01-24 22:50:27 -0800

    Add files via upload

---
+++ Asish John Joshy/21-01-2020/declare_variables.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class declare_variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int x=10;
+		System.out.println(x);
+	}
+}
