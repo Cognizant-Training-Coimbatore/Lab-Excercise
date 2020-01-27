2752fa86df402fb659a8514278645116b2156c48
fakamasaifu1234 <60282998+fakamasaifu1234@users.noreply.github.com>
2020-01-27 01:12:02 -0800

    Add files via upload

---
+++ FakamaSaifunnisa/27jan/qn_3.java
@@ -0,0 +1,23 @@
+package packB;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class qn_3 {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String k=null;
+		try {
+		if(k.equals("abcd")){
+				
+			System.out.println("enteres value");
+		}
+		}
+		catch( NullPointerException m) {
+			System.out.println("error occured "+m);
+		}
+	}
+
+}
