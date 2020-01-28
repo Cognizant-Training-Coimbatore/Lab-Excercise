cf160719bafe03a9ee9623c61832630c8a68ffeb
soumya rao <60283079+soumyarao555@users.noreply.github.com>
2020-01-28 02:49:11 -0800

    Add files via upload

---
+++ Soumya rao/28-01-2020/ex_8.java
@@ -0,0 +1,41 @@
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ex_8{
+		static void fun(Map<Integer, String> hashmap1 )
+		{
+			int x1=1000,x2=0;
+			Set<Integer> a=hashmap1.keySet();
+			for(int x:a)
+			{
+				if(x<x1)
+				{
+					x1=x;
+				}
+				if(x>x2)
+				{
+					x2=x;
+				}
+			}
+			System.out.println("Lowest key element:"+hashmap1.get(x1));
+			System.out.println("Highest key element:"+hashmap1.get(x2));
+		}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<Integer,String> hashmap1=new HashMap<Integer,String>();
+		hashmap1.put(2, "Java");
+		hashmap1.put(1, "C#");
+		hashmap1.put(6, "Selenium");
+		hashmap1.put(4, "Perl");
+		hashmap1.put(5, "Cobol");
+		hashmap1.put(3, "Fortran");
+		fun(hashmap1);
+	}
+
+}
+
---
+++ Soumya rao/28-01-2020/ex_9.java
@@ -0,0 +1,24 @@
+
+
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ex_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<Integer,String> hashmap1=new HashMap<Integer,String>();
+		hashmap1.put(1, "Java");
+		hashmap1.put(2, "C#");
+		hashmap1.put(3, "Selenium");
+		hashmap1.put(4, "Perl");
+		hashmap1.put(5, "Cobol");
+		hashmap1.put(6, "Fortran");
+		System.out.println(hashmap1.size());
+	}
+
+}
+
+
+
