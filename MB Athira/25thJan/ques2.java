2976fe5795d5d2ec35195936d37174f15fb774ae
athiramb2103 <60283056+athiramb2103@users.noreply.github.com>
2020-01-28 09:30:40 +0530

    Add files via upload

---
+++ MB Athira/25thJan/ques2.java
@@ -0,0 +1,21 @@
+package packA;
+
+public class ques2
+{
+int emp_no;
+String empname;
+
+protected  void ques2()
+{
+emp_no=123;
+empname="Della";
+
+
+}
+protected void display1()
+{
+System.out.println("emp_no is"+emp_no);
+System.out.println("empname is"+empname);
+
+}
+}
---
+++ MB Athira/25thJan/ques2_b.java
@@ -0,0 +1,22 @@
+package packB;
+import packA.ques2;
+class test3 extends ques2
+{
+void display2()
+{
+ques2();
+display1();
+}
+
+}
+public class ques2_b {
+
+public static void main(String[] args)
+{
+
+test3 obj=new test3();
+obj.display2();
+}
+
+}
+
