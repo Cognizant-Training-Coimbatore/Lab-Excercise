1426e318926a58b65fa1d188eeb54483d29a5570
fakamasaifu1234 <60282998+fakamasaifu1234@users.noreply.github.com>
2020-01-27 21:44:04 -0800

    Add files via upload

---
+++ FakamaSaifunnisa/28 jan/autoboxing.java
@@ -0,0 +1,13 @@
+
+public class autoboxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int x=100;
+			Integer obj=x;
+			System.out.println(obj.compareTo(200));
+			System.out.println(obj.compareTo(100));
+	}
+
+}
