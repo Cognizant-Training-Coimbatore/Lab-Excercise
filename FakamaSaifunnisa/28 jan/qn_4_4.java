85dc80ad8b14b6d379c2d8b6696f6b1f75a3271b
fakamasaifu1234 <60282998+fakamasaifu1234@users.noreply.github.com>
2020-01-28 00:28:27 -0800

    Add files via upload

---
+++ FakamaSaifunnisa/28 jan/qn_4_4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class qn_4_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> color=new ArrayList<String>();
+		color.add("red");
+		color.add("blue");
+		color.add("violet");
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		String s=color.get(a);
+		System.out.println(s);
+		
+		
+	}
+
+}
