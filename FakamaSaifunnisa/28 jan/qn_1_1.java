9e3e6928639df64fb67bb4088efa8d94cbbfbccd
fakamasaifu1234 <60282998+fakamasaifu1234@users.noreply.github.com>
2020-01-28 00:03:51 -0800

    Add files via upload

---
+++ FakamaSaifunnisa/28 jan/qn_1_1.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class qn_1_1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			List<String> color=new ArrayList<String>();
+			color.add("blue");
+			color.add("orange");
+			for(String x:color) {
+				System.out.println(x);
+			}
+		
+	}
+
+}
+
+
+
