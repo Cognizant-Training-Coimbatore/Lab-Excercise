771aaf8a88892f4bdc1a4e7dd4b029fc8ea5c639
fakamasaifu1234 <60282998+fakamasaifu1234@users.noreply.github.com>
2020-01-28 02:36:32 -0800

    Add files via upload

---
+++ FakamaSaifunnisa/28 jan/qn_16_16.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Scanner;
+public class qn_16_16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<Integer,String> hashmap1=new HashMap<Integer,String>();
+		hashmap1.put(1,"java");
+		hashmap1.put(2,"Selenium");
+		hashmap1.put(3,"C++");
+		hashmap1.put(4,"perl");
+		hashmap1.put(5,"cobol");
+		int s=hashmap1.size();
+		System.out.println("Size is:"+s);
+
+	}
+
+}
