93e020c8df7cf5ed84018be368cdeac31e3c4ee9
kiran28tk <60283071+kiran28tk@users.noreply.github.com>
2020-01-28 13:23:10 +0530

    Add files via upload

---
+++ Kiran K/28-01-2020/autoboxing.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class autoboxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int x =100;
+         Integer obj= x;
+         System.out.println(obj.compareTo(200));
+         System.out.println(obj.compareTo(100));
+         int y=obj;
+         System.out.println(y);
+         System.out.println(Integer.parseInt("123")+2);
+	}
+
+}
---
+++ Kiran K/28-01-2020/enhanced_for_loop.java
@@ -0,0 +1,35 @@
+package demo;
+
+import java.util.ArrayList;
+
+public class enhanced_for_loop {
+     static void printArray(int myarray[])
+     {
+    	 for(int x:myarray)
+    	 {
+    		 System.out.println(x);
+    	 }
+     }
+     static void printArrayList(ArrayList list1)
+     {
+    	 for(Object obj:list1)
+    	 {
+    		 System.out.println(obj);
+    	 }
+     }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int no[]=new int[3];
+        no[0]=12;
+        no[1]=54;
+        no[2]=85;
+        printArray(no);
+        ArrayList mylist=new ArrayList();
+        mylist.add("item1");
+        mylist.add("item2");
+        mylist.add("item3");
+        
+        printArrayList(mylist);
+	}
+
+}
