4c4222f0c2750e4e5e1d05fd22e39485fd3b5c35
Vishnu Santhosh <56623157+vish7d@users.noreply.github.com>
2020-01-28 09:25:46 +0530

    Add files via upload

---
+++ Vishnu Santhosh/25-01-2020/Exer4_Q1.java
@@ -0,0 +1,16 @@
+package packB;
+import packA.emp;
+class empl extends emp{
+	void print() {
+		System.out.println(salary);
+	}
+}
+public class Exer4_Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		empl obj = new empl();
+		obj.print();
+	}
+
+}
---
+++ Vishnu Santhosh/25-01-2020/Exer4_Q3.java
@@ -0,0 +1,10 @@
+package packB;
+
+public class Exer4_Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
