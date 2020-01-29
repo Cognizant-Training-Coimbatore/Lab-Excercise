e7041206a436f765da1efd67abca45a673969ce6
Edwin Francis <edwinfrancis002@gmail.com>
2020-01-28 20:23:42 -0800

    Add files via upload

---
+++ EdwinFrancis/28/keyvaluemap.java
@@ -0,0 +1,20 @@
+package helloworld;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class keyvaluemap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<Integer,String> hashmap1=new HashMap<Integer,String>();
+		hashmap1.put(1, "Java");
+		hashmap1.put(2, "C#");
+		hashmap1.put(3, "Selenium");
+		hashmap1.put(4, "Perl");
+		hashmap1.put(5, "Cobol");
+		hashmap1.put(6, "Fortran");
+		System.out.println(hashmap1.size());
+	}
+
+}
