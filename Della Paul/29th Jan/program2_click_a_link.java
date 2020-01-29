3c30fb2c2f2bb46e5854d64ff7d47583336cfb46
dellapaul1124 <60283029+dellapaul1124@users.noreply.github.com>
2020-01-29 03:50:03 -0800

    Add files via upload

---
+++ Della Paul/29th Jan/program2_click_a_link.java
@@ -0,0 +1,23 @@
+package demowebdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class program2_click_a_link {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://demowebshop.tricentis.com");
+		WebElement login =driver.findElement(By.linkText("Log in"));
+		login.click();
+		System.out.println(driver.getTitle());
+		driver.quit();
+		
+		
+	}
+
+}
