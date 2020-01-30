2d86a3bf168edd4d3f4db73185207a989262da08
avinashkormatha <60282996+avinashkormatha@users.noreply.github.com>
2020-01-30 09:26:26 +0530

    Add files via upload

---
+++ AvinashKormatha/29 jan/program4_click_a_button.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class program4_click_a_button {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demowebshop.tricentis.com");
+		WebElement login = driver.findElement(By.linkText("Log in"));
+		login.click();
+		WebElement username = driver.findElement(By.id("Email"));
+		username.sendKeys("avinash");
+		WebElement submit = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[1]/div[2]/div[2]/form/div[5]/input"));
+	
+		Thread.sleep(5000);
+		
+		driver.quit();
+	}
+
+}
