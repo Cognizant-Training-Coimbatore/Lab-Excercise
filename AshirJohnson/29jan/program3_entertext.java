f7036165affc1b5bb441891814d2ebd80d14669b
ashirjohnson <50360265+ashirjohnson@users.noreply.github.com>
2020-01-30 09:25:50 +0530

    Add files via upload

---
+++ AshirJohnson/29jan/program3_entertext.java
@@ -0,0 +1,23 @@
+package demoWebDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class program3_entertext {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demowebshop.tricentis.com");
+		WebElement login=driver.findElement(By.linkText("Log in"));
+		login.click();
+		WebElement username=driver.findElement(By.id("Email"));
+		username.sendKeys("ashir");
+		Thread.sleep(5000);
+		driver.quit();
+	}
+
+}
