e4a2140ea263c3164dfe0df26aae0120a169b5f2
zenith-kumar <60283032+zenith-kumar@users.noreply.github.com>
2020-01-31 10:33:16 +0530

    Add files via upload

---
+++ zenithkumar/29jan/enter_text_into_textbox.java
@@ -0,0 +1,24 @@
+package demoWebdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class enter_text_into_textbox {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demowebshop.tricentis.com");
+		WebElement login = driver.findElement(By.linkText("Log in"));
+		login.click();
+		WebElement username = driver.findElement(By.id("Email"));
+		username.sendKeys("zenithkumar");
+		Thread.sleep(5000);
+		driver.quit();
+		
+	}
+
+}
---
+++ zenithkumar/29jan/prograam1_launch_browser.java
@@ -0,0 +1,19 @@
+package demoWebdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class prograam1_launch_browser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdrive.chrome.driver","chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demowebshop.tricentis.com");
+		String s = driver.getTitle();
+		System.out.println(s);
+		driver.quit();
+
+	}
+
+}
